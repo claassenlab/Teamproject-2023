44ccc917fabbdb53a0d91300c8d5171de9edb711
Finn Springorum <finn.springorum@live.de>
2023-04-23 12:45:25 +0200

    Feature #2: Set up CI

---
+++ tests/test_1.py
@@ -0,0 +1,9 @@
+# Important note: Every test file and function has to start with test_... so that pytest works properly
+# A good video for more information: https://youtu.be/Ls6n3WZ_tyo?t=94
+
+def test_dummy():
+    """These tests are only there to please the CI. Replace or delete them later."""
+    assert 4 + 2 == 6
+    assert 3 - 1 == 2
+    assert 2 * 4 == 8
+    assert 9 / 3 == 3
